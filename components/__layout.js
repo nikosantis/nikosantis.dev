2f080dfa58ca702c4ebd03fe528a4d8e1d11a4cf
Nikolas Santis <nikosantis@gmail.com>
2020-09-12 22:13:20 -0300

    fix bugs

--- components/__layout.js
+++ components/__layout.js
@@ -1,7 +1,5 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
-import { getPostBySlug, getAllPosts } from 'lib/api'
-import markdownToHtml from 'lib/markdownToHtml'
 import Page from 'components/layout/page'
 import Container from 'components/container'
 import BodyPost from 'components/body-post'
@@ -40,40 +38,4 @@ export default function BlogPost ({ post }) {
       </style>
     </Page>
   )
-}
-
-export async function getStaticProps ({ params }) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'excerpt',
-    'content',
-    'coverImage'
-  ])
-  const content = await markdownToHtml(post.content || '')
-
-  return {
-    props: {
-      post: {
-        ...post,
-        content
-      }
-    }
-  }
-}
-
-export async function getStaticPaths () {
-  const posts = getAllPosts(['slug'])
-
-  return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          slug: post.slug
-        }
-      }
-    }),
-    fallback: false
-  }
 }
