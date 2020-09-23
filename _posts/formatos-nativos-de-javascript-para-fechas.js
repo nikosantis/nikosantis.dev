92f18c67c2b021b58b3597da6ff23c018910996f
Nikolas Santis <nikosantis@gmail.com>
2020-09-23 18:57:26 -0300

    fix cover images

--- _posts/formatos-nativos-de-javascript-para-fechas.js
+++ _posts/formatos-nativos-de-javascript-para-fechas.js
@@ -3,6 +3,6 @@ export const meta = {
   description: 'Por mucho tiempo he usado Momentjs, y si bien ahora uso Luxon, he querido compartir este post para revisar 3 métodos nativos para formatear una fecha y hora.',
   date: 'Marzo 18, 2020',
   dateForISO: '03/18/2020',
-  image: '/images/posts/formatos-nativos-de-javascript-para-fechas/cover.png',
+  image: 'https://nikosantis.dev/images/posts/formatos-nativos-de-javascript-para-fechas/cover.png',
   url: 'formatos-nativos-de-javascript-para-fechas'
 }
--- _posts/se-viene-react-v17.js
+++ _posts/se-viene-react-v17.js
@@ -3,6 +3,6 @@ export const meta = {
   description: 'En el blog oficial de React se ha publicado el primer candidato a lanzamiento para React 17, luego de dos años y medio de un importante lanzamiento anterior.',
   date: 'Agosto 10, 2020',
   dateForISO: '08/10/2020',
-  image: '/images/posts/se-viene-react-v17/cover.png',
+  image: 'https://nikosantis.dev/images/posts/se-viene-react-v17/cover.png',
   url: 'se-viene-react-v17'
 }
