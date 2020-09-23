06920e8ae8bece1c1df67acaceda008e495f7e83
Nikolas Santis <nikosantis@gmail.com>
2020-09-23 19:22:01 -0300

    change url slug fix

--- _posts/la-nueva-transformacion-jsx-de-react.js
+++ _posts/la-nueva-transformacion-jsx-de-react.js
@@ -3,6 +3,6 @@ export const meta = {
   description: 'Si bien React 17 no trae nuevas funciones, brindará soporte para una nueva versión de la transformación JSX.',
   date: 'Septiembre 23, 2020',
   dateForISO: '09/23/2020',
-  image: 'la-nueva-transformacion-jsx/cover.png',
-  url: 'la-nueva-transformacion-jsx'
+  image: 'la-nueva-transformacion-jsx-de-react/cover.png',
+  url: 'la-nueva-transformacion-jsx-de-react'
 }
