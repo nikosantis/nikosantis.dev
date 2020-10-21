80387defdb9ec3fd70481231dd80c581ff1e801f
Nikolas Santis <nikosantis@gmail.com>
2020-10-21 10:10:59 -0300

    react 17 post

---
+++ _posts/react-17-ya-esta-aqui.js
@@ -0,0 +1,8 @@
+export const meta = {
+  title: 'React v17.0 ya está aquí',
+  description: 'Hoy 20 de octubre ha sido lanzado React v17.0 de manera oficial. ¿Ya sabes qué trae esta nueva versión?',
+  date: 'Octubre 20, 2020',
+  dateForISO: '10/20/2020',
+  image: 'react-17-ya-esta-aqui/cover.png',
+  url: 'react-17-ya-esta-aqui'
+}
