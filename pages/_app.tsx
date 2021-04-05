f393c94b95cf45d23681d3dfd560cb86b2385fce
nikosantis <nikosantis@gmail.com>
2021-04-05 13:17:37 -0400

    update refixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/fonts.css'
 import '../styles/base.css'
 import type { AppProps } from 'next/app'
-// import { ThemeProvider } from 'next-themes'
+import { ThemeProvider } from 'next-themes'
 import { MDXProvider } from '@mdx-js/react'
 
 import Progress from 'components/nprogress'
@@ -10,10 +10,10 @@ import { components } from 'components/mdx-components'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MDXProvider components={components}>
-      {/* <ThemeProvider attribute='class' enableSystem={false}> */}
-      <Progress />
-      <Component {...pageProps} />
-      {/* </ThemeProvider> */}
+      <ThemeProvider attribute='class' enableSystem={false}>
+        <Progress />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </MDXProvider>
   )
 }
