3ef10afa2efb15e822a97d3cd86ae969c26c9772
Nikolas Santis <nikosantis@gmail.com>
2021-04-29 16:27:55 -0400

    update next 10.2

--- next.config.js
+++ next.config.js
@@ -8,10 +8,7 @@ const withMDX = require('@next/mdx')({
 })
 
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  future: {
-    webpack5: true
-  }
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
 }
 
 module.exports = withMDX(nextConfig)
